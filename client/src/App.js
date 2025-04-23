eb288a55d1d2cf15868a1b7e829f41d5b3b4b62f
ARMAAN CHANDARANA <achandarana@bayviewglen.ca>
2025-04-23 09:20:28 -0400

    added due date and improved ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
 import React, {useState, useEffect} from "react";
-import { fetchTodos } from "./api";
+import { fetchTodos, addTodo, updateTodo, deleteTodo } from "./api";
+import {Dialog} from './components/dialog.js';
+
+const totoInputItems = [
+    { name: "title", type: "text", label: "Title:" },
+    { name: "due_date", type: "datetime-local", label: "Due Date"},
+]
 
 const App = () => {
     const [todos, setTodos] = useState([]);
+    const [openedTodo, setOpenedTodo] = useState(null);
 
-    useEffect(() => {
-        const getTodos = async () => {
-            console.log("getting todos");
-            try{
-                const res = await fetchTodos();
-                setTodos(res.data);
-                console.log("todos", res.data);
-            } catch (e){
-                console.error("error fetching todos", e);
-            }
+    const getTodos = async () => {
+        console.log("getting todos");
+        try{
+            const res = await fetchTodos();
+            setTodos(res.data);
+            console.log("todos", res.data);
+        } catch (e){
+            console.error("error fetching todos", e);
         }
+    }
+    
+    useEffect(() => {
         getTodos();
     }, []);
 
+    //TABLE STYLING
     const allTableElementStyles = {
         border: '1px solid black',
         borderCollapse: 'collapse',
@@ -43,11 +52,20 @@ const App = () => {
         
     }
 
-
-
     return (
         <div style={{ padding: '2rem' }}>
-            <button>ADD TODO</button>
+            {/* <form onSubmit={async (e) => {
+                e.preventDefault();
+                const formData = Object.fromEntries(new FormData(e.target).entries());
+                await addTodo(formData.title, formData.due_date);
+                await getTodos();
+                e.target.reset();
+            }}>
+                <input type="text" name="title"></input>
+                <input type="datetime-local" name="due_date"></input>
+                <button type="submit">SUBMIT</button>
+            </form> */}
+            <button onClick={() => setOpenedTodo(-1)}>ADD TODO</button>
           <center><h1>My Todo List</h1></center>
           <table style={{...allTableElementStyles, ...tableStyles, ...headerRowStyles}}>
             <tr style={{...allTableElementStyles, ...trStyles}}>
@@ -55,28 +73,74 @@ const App = () => {
                     TITLE
                 </th>
                 <th style={{...allTableElementStyles, ...tdStyles}}>
-                    
+                    DUE DATE
+                </th>
+                <th style={{...allTableElementStyles, ...tdStyles}}>
+                    EDIT
                 </th>
                 <th>
-
+                    MARK DONE
                 </th>
             </tr>
             {todos.map((todo, i) => (
               <tr 
                 key={todo.id} 
                 style={{...allTableElementStyles, ...(i%2==0 ? trEvenStyles : trStyles)}}>
+                {/* TITLE */}
                 <td style={{...allTableElementStyles, ...tdStyles}}>
                     {todo.title}
                 </td>
+
+                {/* DUE DATE */}
                 <td style={{...allTableElementStyles, ...tdStyles}}>
-                    <button>EDIT</button>
+                    {todo.due_date==null ? "Not Provided" : 
+                        new Date(todo.due_date).toLocaleString('en-US', { 
+                            dateStyle: 'long', 
+                            timeStyle: 'short', 
+                        })
+                    }
                 </td>
+
+                {/* EDIT */}
                 <td style={{...allTableElementStyles, ...tdStyles}}>
-                    <button>MARK AS DONE</button>
+                    <button onClick={() => setOpenedTodo(openedTodo==null ? todo.id : null)}>EDIT</button>
+                </td>
+
+                {/* MARK DONE */}
+                <td style={{...allTableElementStyles, ...tdStyles}}>
+                    <button onClick={async () => {
+                        await deleteTodo(todo.id);
+                        await getTodos();
+                    }}>MARK AS DONE</button>
                 </td>
               </tr>
             ))}
           </table>
+            {openedTodo==-1 && 
+                <Dialog 
+                    setShow={setOpenedTodo} 
+                    title={"ADD TODO"}
+                    params={totoInputItems}
+                    onSubmit={async (data) => {
+                        console.log("added submit")
+                        await addTodo(data);
+                        await getTodos();
+                    }}
+                />
+            }
+
+            {(openedTodo!=null && openedTodo>=0) && 
+                <Dialog 
+                    todo={todos.find(todo => todo.id==openedTodo)}
+                    setShow={setOpenedTodo} 
+                    title={"UPDATE"}
+                    params={totoInputItems}
+                    onSubmit={async (data) => {
+                        console.log("update submit")
+                        await updateTodo(openedTodo, data);
+                        await getTodos();
+                    }}
+                />}
         </div>
       );
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//connect the database
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
 });
@@ -16,6 +17,7 @@ client.connect()
     .then(() => console.log("connected to database"))
     .catch((err) => console.log("error connecting to database", err.stack));
 
+//get everything from database ans send to client
 app.get('/api/todos', async (req, res) => {
     try{
         const result = await client.query("SELECT * FROM todos");
@@ -26,8 +28,9 @@ app.get('/api/todos', async (req, res) => {
     }
 });
 
+//add a new todo to the database from the client
 app.post('/api/todos', async (req, res) => {
-    const { title } = req.body;
+    const { title, due_date } = req.body;
 
     if (!title) {
         return res.status(400).json({ error: 'Title is required' });
@@ -35,8 +38,8 @@ app.post('/api/todos', async (req, res) => {
 
     try{
         const result = await client.query(
-            'INSERT INTO todos (title) VALUES ($1) RETURNING *',
-            [title]
+            'INSERT INTO todos (title, due_date) VALUES ($1, $2) RETURNING *',
+            [title, due_date]
         );
         res.status(201).json(result.rows);
     }catch (err){
@@ -45,23 +48,46 @@ app.post('/api/todos', async (req, res) => {
     }
 });
 
+//update the data of a todo
 app.post('/api/todos/update', async (req, res) => {
-    const { id, title} = req.body;
+    const { id, title, due_date} = req.body;
+    console.log("updating todo", id, " title: ", title);
     if (!id) req.status(400).json({error: "ID is required"});
 
     try{
-        const result = client.query(
-            "UPDATE todos SET title = $2 WHERE id = $1 RETURNING *",
-            [id, title]
+        const result = await client.query(
+            "UPDATE todos SET title = COALESCE($2, title), due_date = COALESCE($3, due_date) WHERE id = $1 RETURNING *;",
+            [id, title, due_date]
         );
-        if (result.rowCount==0) req.status(400).json({error: "todo with ID of " + String(id) + " was not found"});
+        if (result.rowCount==0) {
+            req.status(400).json({error: "todo with ID of " + String(id) + " was not found"}); 
+            return ;
+        }
         res.json(result.rows[0]);
     }catch (err){
         console.error('Error updating todo:', err);
         res.status(500).json({ error: 'Error updating todo' });
     }
-})
+});
 
+//delete a todo from the client
+app.delete('/api/todos/delete', async (req, res) => {
+    console.log("deleting a todo");
+    const { id } = req.body;
+    console.log("deleting todo", id);
+    try{
+        const result = await client.query(
+            'DELETE FROM todos WHERE id=$1 RETURNING *',
+            [id]
+        );
+        res.json(result.rows[0])
+    }catch (err){
+        console.log("error deleting todo", err);
+        res.status(500).json({ error: "error deleting todo"})
+    }
+});
+
+//start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("server running on port", PORT);
